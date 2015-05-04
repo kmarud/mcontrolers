7301f441101d39016e00b116a7bf82867b719e68
Kamil Marud <200894@student.pwr.edu.pl>
2015-05-04 13:00:12 +0200

    Poprawiono projekt termometr

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Projekty:
 Odbiornik IR, regulacja obrotów wiatraka przez PWM, wyœwietlacz 7 segmentowy mulipleksowany, wyœwietlanie w % prêdkoœci obrotów wiatraka.
 
 2. termometr :
-Odczyt temperatury poprzez cyfrowy termometr, wynik wyœwietlany na wyœwietlaczu LCD oraz poprzez port szeregowy. Dodatkowo nadajnik IR.
+Odczyt temperatury poprzez cyfrowy termometr, wynik wyœwietlany na wyœwietlaczu LCD oraz poprzez port szeregowy. 
+Dodatkowo nadajnik IR-tryb auto lub manual.
--- termometr/termometr.ino
+++ termometr/termometr.ino
@@ -1,26 +1,26 @@
 #include <OneWire.h>
 #include <LiquidCrystal.h>
-// OneWire DS18S20, DS18B20, DS1822 Temperature Example
-//
-// http://www.pjrc.com/teensy/td_libs_OneWire.html
-//
-// The DallasTemperature library can do all this work for you!
-// http://milesburton.com/Dallas_Temperature_Control_Library
-
-OneWire  ds(10);  // on pin 10 (a 4.7K resistor is necessary)
-LiquidCrystal lcd(12, 11, 7, 6, 5, 4);
 #include <IRremote.h>
 
+
+OneWire  ds(10);
+LiquidCrystal lcd(7, 8, 5, 12, 11, 9);
 IRsend irsend;
 
 
 int zmiennaGlobalna=0;
 int docelowa=30;
-
+volatile boolean automat=true;
+ 
+ 
+ 
 void setup(void) {
   Serial.begin(9600);
-    lcd.begin(16, 2);
-  lcd.print("Temp: ");
+  lcd.begin(16, 2);
+  
+ attachInterrupt(0, ffff, CHANGE);
+  pinMode(A0, INPUT);
+  pinMode(2, INPUT_PULLUP);
 }
 
 void loop(void) {
@@ -29,112 +29,85 @@ void loop(void) {
   byte type_s;
   byte data[12];
   byte addr[8];
-  float celsius, fahrenheit;
-  
+  float celsius;
+
   if ( !ds.search(addr)) {
-    Serial.println("No more addresses.");
-    Serial.println();
-    ds.reset_search();
     delay(250);
     return;
   }
   
-  Serial.print("ROM =");
-  for( i = 0; i < 8; i++) {
-    Serial.write(' ');
-    Serial.print(addr[i], HEX);
-  }
-
-  if (OneWire::crc8(addr, 7) != addr[7]) {
-      Serial.println("CRC is not valid!");
-      return;
-  }
-  Serial.println();
- 
-  // the first ROM byte indicates which chip
-/*  switch (addr[0]) {
-    case 0x10:
-      Serial.println("  Chip = DS18S20");  // or old DS1820
-      type_s = 1;
-      break;
-    case 0x28:
-      Serial.println("  Chip = DS18B20");
-      type_s = 0;
-      break;
-    case 0x22:
-      Serial.println("  Chip = DS1822");
-      type_s = 0;
-      break;
-    default:
-      Serial.println("Device is not a DS18x20 family device.");
-      return;
-  } */
   type_s = 0;
   ds.reset();
   ds.select(addr);
-  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+  ds.write(0x44, 1);        
   
-  delay(1000);     // maybe 750ms is enough, maybe not
-  // we might do a ds.depower() here, but the reset will take care of it.
+  delay(1000);     
+  
   
   present = ds.reset();
   ds.select(addr);    
-  ds.write(0xBE);         // Read Scratchpad
+  ds.write(0xBE); 
 
-  Serial.print("  Data = ");
-  Serial.print(present, HEX);
-  Serial.print(" ");
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+  for ( i = 0; i < 9; i++) {          
     data[i] = ds.read();
-    Serial.print(data[i], HEX);
-    Serial.print(" ");
   }
-//  Serial.print(" CRC=");
-//  Serial.print(OneWire::crc8(data, 8), HEX);
-//  Serial.println();
 
-  // Convert the data to actual temperature
-  // because the result is a 16 bit signed integer, it should
-  // be stored to an "int16_t" type, which is always 16 bits
-  // even when compiled on a 32 bit processor.
   int16_t raw = (data[1] << 8) | data[0];
   if (type_s) {
     raw = raw << 3; // 9 bit resolution default
     if (data[7] == 0x10) {
-      // "count remain" gives full 12 bit resolution
       raw = (raw & 0xFFF0) + 12 - data[6];
     }
   } else {
     byte cfg = (data[4] & 0x60);
-    // at lower res, the low bits are undefined, so let's zero them
     if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
     else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
     else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
     //// default is 12 bit resolution, 750 ms conversion time
   }
   celsius = (float)raw / 16.0;
-  fahrenheit = celsius * 1.8 + 32.0;
-  Serial.print("  Temperature = ");
-  lcd.setCursor(6, 0);
+  
+  docelowa=analogRead(A0);
+  docelowa=map(docelowa,0,1023,20,40);
   Serial.print(celsius);
+  
+  lcd.home();
+  lcd.print("Temp: ");
+  lcd.setCursor(8, 0);
+  
   lcd.print(celsius);
   lcd.write(223);
   lcd.print("C");
+  if(automat==true)
+  {
+  Serial.print(" , tryb auto\n");
   lcd.setCursor(0, 1);
-  lcd.print("docelowa: ");
- // lcd.print(millis()/1000);
-  Serial.print(" Celsius, ");
-  Serial.print(fahrenheit);
-  Serial.println(" Fahrenheit");
+  lcd.print("Zadana: ");
+  lcd.print(docelowa);
+   lcd.write(223);
+  lcd.print("C");
+  }
+  else
+  {
+    Serial.print("\n");
+     lcd.setCursor(0, 1);
+     lcd.print("                ");
+  }
   zmiennaGlobalna++;
-   if(celsius<docelowa && zmiennaGlobalna>=5)
+   if((int)celsius<docelowa && zmiennaGlobalna>=5 && automat==true)
    {
      irsend.sendSony(0x937BB355,32); // Sony TV power code
      zmiennaGlobalna=0;
    }
-   else if(celsius>docelowa && zmiennaGlobalna>=5)
+   else if((int)celsius>docelowa && zmiennaGlobalna>=5 && automat==true)
    {
      irsend.sendSony(0xD1D4CC60,32); // Sony TV power code
      zmiennaGlobalna=0;
    }
+}
+
+
+void ffff()
+{
+  automat=digitalRead(2);
 }
